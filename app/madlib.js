974779d3a688509ede9471194aadb4f3052896b8
jwang49 <jwang49@cs.brown.edu>
2018-02-14 10:20:25 -0500

    stencil

---
+++ app/madlib.js
@@ -0,0 +1,104 @@
+/********************************************************************************/
+/*										*/
+/*	Initial set of madlibs.  Please add your own.				*/
+/*										*/
+/********************************************************************************/
+
+var madlibs = [
+    {
+		title : "More Lights!",
+		author : "unknown",
+		content : 'Did you ever have one of those [nouns] ? Well today [person] did! Mom ' +
+			'wanted to [verb] [nouns] up for [holiday] . Not just any lights, ' +
+			'[color] lights. [Adjective] [color] lights! [Adjective] bright red ' +
+			'lights. The only problem, they are a [verb/ed] mess! Not to mention ' +
+			'that there are some [vegetable], yellow and green lights [verb/ed] ' +
+			'in. " [person] !" I yell! This can\'t be done! She could [verb] I was ' +
+			'right and went out and [verb] some new shiny [color] lights!'
+	},
+    {
+		title: 'Christmas Tree',
+		author: 'unknown',
+		content: 'Every [month] we [verb] to a tree [place] far away. not just any ' +
+			'[adjective] farm, a [adjective] tree [place] . My dad and I [verb] ' +
+			'onto the [noun] to [verb] for the perfect [noun]. Some people live ' +
+			'[adjective] and [adjective] and some like them [color] and fat. We ' +
+			'are searching for a tall and [adjective] one! "Over there!" I ' +
+			'exclaim, "Dad it\'s over there!" Off we [verb], saw in hand to [verb] ' +
+			'this year\'s [noun] down. [Exclamation] it\'s [holiday] finally!'
+	},
+    {
+		title: 'Turkey Dinner!',
+		author: 'unknown',
+		content: 'I spent last summer on my grandfather\'s [adjective] farm. He raises ' +
+			'[noun/s] for local food [noun/s] . He also grows corn on the [noun], ' +
+			'[adjective] lettuce and lima [noun/s] . My favorite place to [verb] ' +
+			'on the farm is the [adjective] house where grandfather keeps his ' +
+			'[noun/s] . But when I visit in November, there are no [noun/s] ! They ' +
+			'are all gone! I ansxiously await at the table that Thanksgiving. I ' +
+			'see corn on the [noun] and eve the lime [noun/s] . I am relieved when ' +
+			'grandma brings out a [noun] for Thanksgiving dinner!'
+	},
+  	{
+		title: 'Pizza Pizza',
+		author: 'unknown',
+		content: 'Pizza was invented by a [adjective] [nationality] chef named [person] . ' +
+			'To make a pizza, you need to take a lump of [noun] , and make a thin, ' +
+			'round [adjective] [noun] . Then you cover it with [adjective] sauce, ' +
+			'[adjective] chees, and fresh chopped [noun/s] . Next you have to bake ' +
+			'it in a very hot [noun] . When it is done, cut it into [number] [shapes] . ' +
+			'Some kids like [food] pizza the best, but my favorite is the [food] pizza. ' +
+			'If I could, I would eat pizza [number] times a day!'
+	},
+	{
+		title: 'Unknown Cyber Beast',
+		author: 'unknown',
+		content: 'Running through Cyberspace, the team encounters a [adjective] [noun].  Unsure of ' +
+			'what to do, they all [verb] and [verb] in order to cause confusion.  However, doing so ' + 
+			'attracted more of its kind.  The team then thinks of a [adjective] plan with hopes for ' +
+			'success.  Unfortunately, [noun] gets caught in the process and the team needs to devise ' +
+			'a new, [adjective] plan in order to save their team member as well as make a [adjective] ' +
+			'escape!'
+	},
+	{
+	   title: 'Working with Triangles',
+	   author: 'unknown',
+	   content: 'Hacker is at it again and he tries to convince that team that any [number] ' +
+	   '[plural noun] can form a triangle.  With this triangle, Hacker wants the team to believe ' +
+	   'that Motherboard can be [verb].  However, the team is [adjective] and calls Hacker out ' +
+	   'on his [adjective] statement, thus challenging his credibility.  However, Hacker will not ' +
+	   '[verb] until after the public has voted for its new [noun].  After proving Hacker as a [noun] ' +
+	   'his [adjective] [noun] come out and the public sees him for what he truly is.'
+	},
+	{
+		title: 'A Tall Tale',
+		author: 'unknown',
+		content: '[number] years ago, Motherboard was brought to life.  Motherboard could [verb] ' +
+			'and [verb] like no other computer at the time.  Motherboard is the [adjective] [noun] of ' +
+			'cyberspace and is its sworn [noun] and will [verb] no matter the cost in order to [verb] ' +
+			'cyberspace.  Whenever Motherboard would get sick with the [noun], cyberspace would become ' +
+			'[adjective].  It would be up to the team to [verb] any situations throughout cyberspace ' +
+			'and sometimes even to find a [noun] for Motherboard.'
+	},
+	{
+		title: 'Black Hole',
+		author: 'unknown',
+		content: 'There is a Black Hole that is [verb] by a sentry named [noun] who charges ' +
+			'disposal fees for objects [verb] into [adjective] the hole. The rate is based on his ' +
+			'appraisal of the object\'s [noun]. The black hole will not dispose of any unpaid items. ' +
+			'This Black Hole is also a [noun] to the [adjective] [noun] of Cyberspace.'
+	},
+	{
+		title: 'Job',
+		author: 'unknown',
+		content: 'Yesterday a [occupation] named [noun] visited MotherBoard and the team to ' +
+			'tell us about a [adjective] job that needs to be done. The most important [noun] needed ' +
+			'to complete the job is to be able to [verb] and [verb]. After listening the job proposal,' +
+			'the team [verb] and [adverb] [verb] to this vistor that they would [verb] the job.  The ' +
+			'visitor was [adjective] at the team\'s response.  While the team [verb] the base with the ' +
+			'visitor to a new [adjective] cybersite.'
+	}
+
+]
+
+module.exports = madlibs;
